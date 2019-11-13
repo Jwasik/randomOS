dea64923c1ba1841165339496ace5407959c6692
CE3DA6C <56481189+CE3DA6C@users.noreply.github.com>
2019-11-14 00:19:18 +0100

    Dodane metody get...

--- randomOS/semaphore.h
+++ randomOS/semaphore.h
@@ -1,5 +1,4 @@
 #pragma once
-#include <atomic>
 #include <mutex>
 #include <memory>
 #include <queue>
@@ -8,20 +7,24 @@
 class PCB {
 
 };
-PCB pcb; //TODO: 
+std::shared_ptr<PCB> pcbPtr; 
 /*DELETE once ProcessManagement.h is ready*/
 
-class Semaphore 
+class Semaphore //TODO: output for step-by-step execution
 {
 private:
-	std::mutex mtx;
-	std::atomic<int> value;
-	std::queue<PCB*> list; //TODO: possible changes due to PCB implementation; change to std::shared_ptr
+	std::mutex mtx; //mutex locks provide protection for the constructor, wait() and signal() methods without negative impacts for multithreaded systems
+	int value;
+	std::queue<std::shared_ptr<PCB>> list; //TODO: possible changes due to PCB implementation
 
-	void sleep(); //TODO: update to reflect the PCB implementation, might be REDUNDANT
-	void wakeup(std::shared_ptr<PCB> w_pcb); //update to reflect the PCB implementation, might be REDUNDANT
+	void sleep(); //TODO: redundant once PCB is implemented
+	void wakeup(std::shared_ptr<PCB> pcbPtr); //redundant once PCB is implemented
+
 public:
 	Semaphore(int new_value);
-	void wait(); //TODO: uninterrupted execution
+	void wait();
 	void signal();
+
+	const int& getValue();
+	const std::queue<std::shared_ptr<PCB>>& getList();
 };
