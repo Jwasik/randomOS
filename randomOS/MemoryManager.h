ce700d27545d4da20ca4592c04dd1a932f6247bb
Szwarc99 <51268010+Szwarc99@users.noreply.github.com>
2019-11-23 15:52:44 +0100

    Add files via upload

--- randomOS/MemoryManager.h
+++ randomOS/MemoryManager.h
@@ -2,17 +2,21 @@
 #include "Includes.h"
 class Memory 
 {
-
+	struct Frame
+	{
+		int PageNr;
+		bool taken=0;
+	};
 
 public:
 	char ram[128];//pamiec wlasciwa
 	Memory(int);
-	void writeInMem(int,std::string);
-	void deleteFromMem(int);
+	void writeInMem(int,std::string);//wpisuje max 16 bajtów do ramki, czeka na zmiany jak bede dostawal page do wpisania
+	void deleteFromMem(int);//czyscze ramke
 	void printMemory();
-	int LogicaltoPhysical(int logical);
-	char& getMemoryContent(int logical, int PC);//dopóki nie wiem jakie adresy dostane od wirtualnej traktuje PID jako adres logiczny
-	bool FrameTable[8]{0};
+	int LogicaltoPhysical(int logical);//zamieniam nr na poczatkowy adres frame'a
+	char& getMemoryContent(int logical, int PC);//dopóki nie wiem jakie adresy dostane traktuje PID jako adres logiczny
+	Frame FrameTable[8];// informacja czy ramka jest zajêta i jaka strona jest w niej wpisana
 	
 
 };
