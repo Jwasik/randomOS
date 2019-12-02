8aa2614190cbbfe4753967351491e9afea515813
Michał Najborowski <michal.najborowski@rsqtechnologies.com>
2019-12-02 22:54:23 +0100

    minor refactor + comments

--- randomOS/VirtualMemory.cpp
+++ randomOS/VirtualMemory.cpp
@@ -21,19 +21,20 @@ void VirtualMemory::Page::print() const {
     std::cout << std::endl;
 }
 
-void VirtualMemory::insertProgram(std::pair<int, std::vector<Page>> process) {
-    swapFile.insert(process);
+void VirtualMemory::insertProgram(std::pair<int, std::vector<Page>> program) {
+    swapFile.insert(program);
 }
 
 void VirtualMemory::updateQueue(int frameNumber) {
-    queue[frameNumber].second = true;
+    for (auto &frame : queue)
+        if (frame.first == frameNumber) frame.second = true;
 }
 
 void VirtualMemory::updateSwapFilePage(int pid, int pageNumber, VirtualMemory::Page page) {
-    for (auto &process : swapFile) {
-        if (process.first == pid)
+    for (auto &program : swapFile) {
+        if (program.first == pid)
             for (int i = 0; i < sizeof(page.data); i++)
-                process.second[pageNumber].data[i] = page.data[i];
+                program.second[pageNumber].data[i] = page.data[i];
     }
 }
 
@@ -58,11 +59,11 @@ VirtualMemory::Page VirtualMemory::getPage(int pid, int pageNumber) {
 
 void VirtualMemory::printSwapFile() {
     std::cout << "---------- SWAP FILE ----------" << std::endl;
-    for (auto &process : swapFile) {
-        std::cout << "PID: " << process.first << std::endl;
-        for (auto &page : process.second)
+    for (auto &program : swapFile) {
+        std::cout << "PID: " << program.first << std::endl;
+        for (auto &page : program.second)
             page.print();
-        std::cout << std::endl;
+        //std::cout << std::endl;
     }
 }
 
