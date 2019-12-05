6572af3d69f7bba1c8f0a55f6328f856d89199e4
Szwarc99 <51268010+Szwarc99@users.noreply.github.com>
2019-12-05 17:49:09 +0100

    Add files via upload

--- randomOS/MemoryManager.h
+++ randomOS/MemoryManager.h
@@ -7,7 +7,7 @@ class Memory
 	{
 		
 		int pid;
-		int PageNr;
+		int ProcessPageNr;
 		bool taken=0;
 		int lastUse=0;
 	};
@@ -16,7 +16,7 @@ public:
 	uint8_t ram[128];//pamiec wlasciwa
 	Memory();
 	int findFreeFrame();//znajduje wolna ramke
-	void writeInMem(uint8_t data [],int pid,int PageNr);//wpisuje strone do ramki, czeka na zmiany jak bede dostawal page do wpisania
+	void writeInMem(uint8_t data,int pid, int logical);//wpisuje strone do ramki, czeka na zmiany jak bede dostawal page do wpisania
 	void deleteFromMem(int);//czyscze ramke
 	void printMemory();
 	uint8_t& getMemoryContent(int logical, int pid);//zwracam bajt po adresie logicznym
