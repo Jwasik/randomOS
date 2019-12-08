c47a2526433b5d6edf4ddd9209546830f294c246
Szwarc99 <51268010+Szwarc99@users.noreply.github.com>
2019-12-08 12:39:29 +0100

    Add files via upload

--- randomOS/MemoryManager.h
+++ randomOS/MemoryManager.h
@@ -1,26 +1,31 @@
+﻿
 #pragma once
 #include "Includes.h"
 #include "VirtualMemory.h"
-class Memory  
+class Memory
 {
 	struct Frame
 	{
-		
+
 		int pid;
 		int ProcessPageNr;
-		bool taken=0;
-		int lastUse=0;
+		bool dirtyflag = 0;
+		int lastUse = 0;
 	};
 
-public:
-	uint8_t ram[128];//pamiec wlasciwa
-	Memory();
-	int findFreeFrame();//znajduje wolna ramke
-	void writeInMem(uint8_t data,int pid, int logical);//wpisuje strone do ramki, czeka na zmiany jak bede dostawal page do wpisania
+	int8_t ram[128]{ NULL };//pamiec wlasciwa
 	void deleteFromMem(int);//czyscze ramke
-	void printMemory();
-	uint8_t& getMemoryContent(int logical, int pid);//zwracam bajt po adresie logicznym
-	Frame FrameTable[8];// informacja czy ramka jest zaj�ta i jaka strona jest w niej wpisana
-	
+	Frame FrameTable[8];// informacja czy ramka jest zajêta i jaka strona jest w niej wpisana
 
+
+public:
+	
+	Memory();
+	//int findFreeFrame();//znajduje wolna ramke, juz niepotrzebne
+	void writeInMem(int8_t data, int pid, int logical);//wpisuje bajt do ramki, czeka na zmiany jak bede dostawal page do wpisania
+	void printMemory();
+	int8_t& getMemoryContent(int pid, int logical);//zwracam bajt po adresie logicznym
+	static VirtualMemory getVM();
+	void test();
+	
 };
