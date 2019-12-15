cc856b0ce67d2a1c8f52200244958defac67d43b
Szwarc99 <51268010+Szwarc99@users.noreply.github.com>
2019-12-15 14:36:11 +0100

    Add files via upload

--- randomOS/MemoryManager.h
+++ randomOS/MemoryManager.h
@@ -1,4 +1,3 @@
-﻿
 #pragma once
 #include "Includes.h"
 #include "VirtualMemory.h"
@@ -12,20 +11,25 @@ class Memory
 		bool dirtyflag = 0;
 		int lastUse = 0;
 	};
-
-	int8_t ram[128]{ NULL };//pamiec wlasciwa
-	void deleteFromMem(int);//czyscze ramke
-	Frame FrameTable[8];// informacja czy ramka jest zajêta i jaka strona jest w niej wpisana
+	//pamiec wlasciwa
+	int8_t ram[128]{ 0 };
+	//czyscze ramke
+	void deleteFromMem(int);
+	// informacja czy ramka jest zajeta i jaka strona jest w niej wpisana
+	Frame FrameTable[8];
 
 
 public:
-	
+
 	Memory();
-	//int findFreeFrame();//znajduje wolna ramke, juz niepotrzebne
-	void writeInMem(int8_t data, int pid, int logical);//wpisuje bajt do ramki, czeka na zmiany jak bede dostawal page do wpisania
+	//wpisuje bajt do ramki, czeka na zmiany jak bede dostawal page do wpisania
+	void writeInMem(int8_t data, int pid, int logical);
 	void printMemory();
-	int8_t& getMemoryContent(int pid, int logical);//zwracam bajt po adresie logicznym
-	static VirtualMemory getVM();
+	//przeglada wszystkie ramki, jesli znajdzie to zwraca jej adres.
+	//jesli nie ma jej w RAMie to
+	//    probuje sprowadzic ja z pliku wymiany
+	//     jesli jest to ja laduje, ale jesli jej nie ma rzucam wyjatek o zlym adresie logicznym
+	int8_t& getMemoryContent(int pid, int logical);
 	void test();
-	
+
 };
