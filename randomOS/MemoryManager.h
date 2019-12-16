ccb0f4cbf5029a1596dcd24e1a00055f37e8c930
Szwarc99 <51268010+Szwarc99@users.noreply.github.com>
2019-12-16 16:18:08 +0100

    Add files via upload

--- randomOS/MemoryManager.h
+++ randomOS/MemoryManager.h
@@ -1,29 +1,47 @@
 #pragma once
+
+
 #include "Includes.h"
 #include "VirtualMemory.h"
 class Memory
 {
 	struct Frame
 	{
-
 		int pid;
-		int ProcessPageNr;
 		bool dirtyflag = 0;
-		int lastUse = 0;
 	};
+	
 	//pamiec wlasciwa
 	int8_t ram[128]{ 0 };
 	//czyscze ramke
 	void deleteFromMem(int);
 	// informacja czy ramka jest zajeta i jaka strona jest w niej wpisana
-	Frame FrameTable[8];
+	Frame FrameTable[8]{
 
+		
+			
+	};
+	std::vector<std::pair<int, bool>> ProcessPages
+	{
+		{-1, 0},
+		{-1, 0},
+		{-1, 0},
+		{-1, 0},
+		{-1, 0},
+		{-1, 0},
+		{-1, 0},
+		{-1, 0}
+	};
 
 public:
-
 	Memory();
+	std::map<int, std::vector<std::pair<int, bool>>> PageTable;
+	void printPageTable(int);
+	
+	
+	
 	//wpisuje bajt do ramki, czeka na zmiany jak bede dostawal page do wpisania
-	void writeInMem(int8_t data, int pid, int logical);
+	void writeInMem(int pid, int logical,int8_t data);
 	void printMemory();
 	//przeglada wszystkie ramki, jesli znajdzie to zwraca jej adres.
 	//jesli nie ma jej w RAMie to
