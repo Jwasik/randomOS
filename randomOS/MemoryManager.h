ec52685401755470832ed791d5b2f893f198fadf
Szwarc99 <51268010+Szwarc99@users.noreply.github.com>
2019-12-17 16:06:49 +0100

    Add files via upload
    linijka 48 cpp nie działa

--- randomOS/MemoryManager.h
+++ randomOS/MemoryManager.h
@@ -12,7 +12,7 @@ class Memory
 	};
 	
 	//pamiec wlasciwa
-	int8_t ram[128]{ 0 };
+	int8_t ram[128];
 	//czyscze ramke
 	void deleteFromMem(int);
 	// informacja czy ramka jest zajeta i jaka strona jest w niej wpisana
@@ -42,13 +42,13 @@ public:
 	
 	
 	//wpisuje bajt do ramki, czeka na zmiany jak bede dostawal page do wpisania
-	void writeInMem(int pid, int logical,int8_t data);
+	uint8_t writeInMem(int pid, int logical,int8_t data);
 	void printMemory();
 	//przeglada wszystkie ramki, jesli znajdzie to zwraca jej adres.
 	//jesli nie ma jej w RAMie to
 	//    probuje sprowadzic ja z pliku wymiany
 	//     jesli jest to ja laduje, ale jesli jej nie ma rzucam wyjatek o zlym adresie logicznym
-	int8_t& getMemoryContent(int pid, int logical);
+	std::pair<uint8_t, int8_t&> getMemoryContent(int pid, int logical);
 	void test();
 
 };
