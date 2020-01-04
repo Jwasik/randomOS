56efa3bad252fbf8719fe2da8616c5e611cedf26
mateusz-stelmasiak <46268673+mateusz-stelmasiak@users.noreply.github.com>
2020-01-04 18:15:47 +0100

    Ujemne wartości w hexa

--- randomOS/Shell.cpp
+++ randomOS/Shell.cpp
@@ -959,10 +959,14 @@ void Shell::printCode(uint8_t code)
 }
 
 template <typename I> std::string Shell::toHexString(I w) {
+
+	I temp = abs(w);
 	size_t hex_len = sizeof(I) << 1;
 	static const char* digits = "0123456789ABCDEF";
 	std::string rc(hex_len, '0');
 	for (size_t i = 0, j = (hex_len - 1) * 4; i < hex_len; ++i, j -= 4)
-		rc[i] = digits[(w >> j) & 0x0f];
-	return rc;
+		rc[i] = digits[(temp >> j) & 0x0f];
+
+	if (w < 0) { return "-" + rc; }
+	return "0"+rc;
 }
