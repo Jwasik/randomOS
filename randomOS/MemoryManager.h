ca0573d743d22375cae6ae648d43f9caecde109b
Szwarc99 <51268010+Szwarc99@users.noreply.github.com>
2020-01-05 16:16:38 +0100

    Add files via upload
    Error gdy podamy zły PID

--- randomOS/MemoryManager.h
+++ randomOS/MemoryManager.h
@@ -1,8 +1,8 @@
 #pragma once
-
-
 #include "Includes.h"
+#include "Page.h"
 #include "VirtualMemory.h"
+
 class Memory
 {
 	struct Frame
@@ -11,44 +11,40 @@ class Memory
 		int page;
 		bool dirtyflag = 0;
 	};
-	
+
 	//pamiec wlasciwa
 	int8_t ram[128];
 	//czyscze ramke
 	void deleteFromMem(int);
 	// informacja czy ramka jest zajeta i jaka strona jest w niej wpisana
-	Frame FrameTable[8]{
+	Frame FrameTable[8];
 
-		
-			
-	};
-	
 	std::shared_ptr<VirtualMemory> vm;
 public:
 	Memory();
 	Memory(std::shared_ptr<VirtualMemory>);
 	void creatProcess(int pid);
-	
-	
-	
+
+
+
 	std::vector<std::pair<int, bool>> ProcessPages
 	{
-		{-1, 0},
-		{-1, 0},
-		{-1, 0},
-		{-1, 0},
-		{-1, 0},
-		{-1, 0},
-		{-1, 0},
-		{-1, 0}
+		{ -1, 0 },
+		{ -1, 0 },
+		{ -1, 0 },
+		{ -1, 0 },
+		{ -1, 0 },
+		{ -1, 0 },
+		{ -1, 0 },
+		{ -1, 0 }
 	};
 	std::map<int, std::vector<std::pair<int, bool>>> PageTable;
 	void printPageTable(int);
-	
-	
-	
+
+
+
 	//wpisuje bajt do ramki, czeka na zmiany jak bede dostawal page do wpisania
-	uint8_t writeInMem(int pid, int logical,int8_t data);
+	uint8_t writeInMem(int pid, int logical, int8_t data);
 	void printMemory();
 	//przeglada wszystkie ramki, jesli znajdzie to zwraca jej adres.
 	//jesli nie ma jej w RAMie to
@@ -57,4 +53,5 @@ public:
 	std::pair<uint8_t, int8_t&> getMemoryContent(int pid, int logical);
 	void test();
 
+	friend class Shell;
 };
