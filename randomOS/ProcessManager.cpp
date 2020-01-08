65ca74e9465de19626fbdea410371a09ff7a7789
mateusz-stelmasiak <46268673+mateusz-stelmasiak@users.noreply.github.com>
2020-01-08 19:06:55 +0100

    TOMKU PATRZ

--- randomOS/ProcessManager.cpp
+++ randomOS/ProcessManager.cpp
@@ -117,9 +117,8 @@ bool ProcessManager::deleteProcess(std::shared_ptr<PCB> process, const std::shar
 		//if the process doesn't have children it can simply be deleted
 		else
 		{
-			
-			fileManager->closeProcessFiles(process->getPID());
 			scheduler->deleteProcess(process->getPID());
+			fileManager->closeProcessFiles(process->getPID());
 			process->getParentPCB()->removeChild(process);
 			//not sure if needed
 			//virtualMemory->removeProgram(process->getPID());
